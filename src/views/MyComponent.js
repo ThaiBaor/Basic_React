c53854e6ee74d11ead05f0273fdd3c3276d4dd67
ThaiBaor <ngphanthaibao203@gmail.com>
2023-12-13 13:32:06 +0700

    Learn State

--- src/views/MyComponent.js
+++ src/views/MyComponent.js
@@ -1,11 +1,23 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+
+    state = {
+        value: 'World',
+    }
+    onClick = () => {
+        this.setState({ value: 'Bao' });
+    }
     render() {
         return (
-            <div>
-                This is MyComponent.
-            </div>
+            <>
+                <div className="firstDiv">
+                    <button onClick={this.onClick}>Click</button>
+                </div>
+                <div className="secondDiv">
+                    Hello {this.state.value}
+                </div>
+            </>
         )
     }
 }
